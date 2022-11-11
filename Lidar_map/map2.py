c96c511855f4c86bdcea08b41b623dd88ed9be4e
Andrey RTS <razorvin.andrey2000@gmail.com>
2022-11-11 12:01:37 +0300

    refactor map22

--- Lidar_map/map2.py
+++ Lidar_map/map2.py
@@ -19,6 +19,7 @@ All text above must be included in any redistribution.
 from math import cos, sin, pi, floor
 import pygame
 from rplidar import RPLidar
+import matplotlib.pyplot as plt
 
 # Set up pygame and the display
 pygame.init()
@@ -48,6 +49,28 @@ def process_data(data):
             point = (360 + int(x / max_distance * 300), 320 + int(y / max_distance * 300))
             lcd.set_at(point, pygame.Color(255, 255, 255))
     pygame.display.update()
+def process_data_new(data):
+    global max_distance
+    plt.scatter(0, 0, label='robot')
+    plt.grid()
+    plt.legend()
+    # plt.xlim([-5, 5])
+    # plt.ylim([-7, 2])
+    plt.ion()
+
+    for angle in range(360):
+        distance = data[angle]
+        if distance > 0:                  # ignore initially ungathered data points
+            radians = angle * pi / 180.0
+            x = distance * cos(radians)
+            y = distance * sin(radians)
+            plt.plot([x, y], linewidth=1, c='blue')
+            plt.xlabel('X, m')
+            plt.ylabel('Y, m')
+            plt.draw()
+
+    plt.ioff()
+    plt.show()
 
 
 scan_data = [0]*360
@@ -57,7 +80,7 @@ try:
         scan_data = [0] * 360
         for (_, angle, distance) in scan:
             scan_data[min([359, floor(angle)])] = distance
-        process_data(scan_data)
+        process_data_new(scan_data)
 
 except KeyboardInterrupt:
     print('Stoping.')
